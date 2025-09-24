Fixed wrong normalization

src/tabpfn/architectures/base/attention/full_attention.py
@@ -697,51 +697,51 @@ class MultiHeadAttention(Attention):
                     share_kv_across_n_heads,
                 )
 
-            block_mask = None
-            block_size = 32
+            # block_mask = None
+            # block_size = 32
             
-            if q.shape[1] == k.shape[1]:
+            # if q.shape[1] == k.shape[1]:
                 
-                # print(f"Fixed Shape of Q: {q.shape}, Shape of K: {k.shape}")
+            #     # print(f"Fixed Shape of Q: {q.shape}, Shape of K: {k.shape}")
             
-                seq_len_q = q.transpose(1, 2).shape[2] 
-                seq_len_k = k.transpose(1, 2).shape[2] if k is not None else seq_len_q  
+            #     seq_len_q = q.transpose(1, 2).shape[2] 
+            #     seq_len_k = k.transpose(1, 2).shape[2] if k is not None else seq_len_q  
                 
                 
                 
-                def causal_mask(b, h, q_idx, kv_idx):
-                    return q_idx < kv_idx
+            #     def causal_mask(b, h, q_idx, kv_idx):
+            #         return q_idx < kv_idx
                 
-                num_blocks_q = seq_len_q // block_size
+            #     num_blocks_q = seq_len_q // block_size
 
-                # print(f"Device: {k.device}")
+            #     # print(f"Device: {k.device}")
 
-                block_mask = create_block_mask(causal_mask, 1, 1, seq_len_q, seq_len_q, device = k.device, BLOCK_SIZE = block_size)
+            #     block_mask = create_block_mask(causal_mask, 1, 1, seq_len_q, seq_len_q, device = k.device, BLOCK_SIZE = block_size)
                 
                
-            # print(f"Finished masking.")   
-            if not block_mask :
-                print("block mask was skipped")
+            # # print(f"Finished masking.")   
+            # if not block_mask :
+            #     print("block mask was skipped")
               
-            attention_head_outputs = flex_attention(
+            # attention_head_outputs = flex_attention(
+            #     q.transpose(1, 2),
+            #     k.transpose(1, 2),
+            #     v.transpose(1, 2),
+            #     block_mask=block_mask,
+            #     # dropout_p=dropout_p,   NO DROPOUT??
+            #     **extra_inputs
+            #     )
+            
+            # del block_mask
+            
+            attention_head_outputs = torch.nn.functional.scaled_dot_product_attention(
                 q.transpose(1, 2),
                 k.transpose(1, 2),
                 v.transpose(1, 2),
-                block_mask=block_mask,
-                # dropout_p=dropout_p,   NO DROPOUT??
-                **extra_inputs
-                )
-            
-            del block_mask
-            
-            # attention_head_outputs = torch.nn.functional.scaled_dot_product_attention(
-            #     q.transpose(1, 2).to("cpu"),
-            #     k.transpose(1, 2).to("cpu"),
-            #     v.transpose(1, 2).to("cpu"),
-            #     # attn_mask=attn_mask,
-            #     dropout_p=dropout_p,
-            #     **extra_inputs,
-            # ).to(k.device)
+                # attn_mask=attn_mask,
+                dropout_p=dropout_p,
+                **extra_inputs,
+            ).to(k.device)
             attention_head_outputs = attention_head_outputs.transpose(1, 2)
         else:
             k = MultiHeadAttention.broadcast_kv_across_heads(k, share_kv_across_n_heads)

src/tabpfn/preprocessors/remove_highly_correlated.py
@@ -42,12 +42,16 @@ class RemoveHighlyCorrelatedFeaturesStep(FeaturePreprocessingTransformerStep):
         if isinstance(X, torch.Tensor):
             x_norm = F.normalize(X, p=2, dim=0).numpy() 
         else:
-            x_norm = np.linalg.norm(X, axis=0)
+            def normalize_np(X, axis=0, p=2, eps=1e-12):
+                norm = np.linalg.norm(X, ord=p, axis=axis, keepdims=True)
+                norm = np.maximum(norm, eps)  # prevent division by zero
+                return X / norm
+            x_norm = normalize_np(X, axis=0)
         
         # Transpose to treat columns as vectors: shape (n_features, n_samples)
         vectors = x_norm.T
 
-        print(f"Dimensionality of data for correlation removal: {vectors.shape}, Shape of X: {X.shape}")
+        # print(f"Dimensionality of data for correlation removal: {vectors.shape}, Shape of X: {X.shape}")
         d = vectors.shape[1]
         
         sel_ = [True] * vectors.shape[0]